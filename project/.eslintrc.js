5e1fed43612148e67a51db96c3dd18cb47979c7a
qtsun <838280707@qq.com>
2021-09-15 20:00:37 +0800

    8.5

---
+++ project/.eslintrc.js
@@ -0,0 +1,24 @@
+const path = require('path')
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  extends: [
+    'plugin:vue/vue3-essential',
+    '@vue/standard'
+  ],
+  parserOptions: {
+    parser: '@babel/eslint-parser',
+    parserOptions: {
+      sourceType: 'module',
+      babelOptions: {
+        configFile: path.join(__dirname, 'babel.config.json')
+      }
+    }
+    },
+    rules: {
+      'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+      'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    }
+  }
---
+++ project/src/App.vue
@@ -0,0 +1,92 @@
+<template>
+  <div>
+    <div class="wrapper">
+    <div class="position">
+    <span class="iconfont position__icon">&#xe6fa;</span>
+    北京理工大学国防科技园2号楼10层北京理工大学国防科技园2号楼10层北京理工大学国防科技园2号楼10层
+    <span class="iconfont position_notice">&#xe7e5;</span>
+    </div>
+  </div>
+    <div class="docker">
+    <!-- BEM
+    block__element--Modifier -->
+      <div class="docker__item docker__item--active">
+        <div class="iconfont">&#xe9ce;</div>
+        <div class="docker__title">首页</div>
+        </div>
+      <div class="docker__item">
+        <div class="iconfont">&#xe6e6;</div>
+        <div class="docker__title">购物车</div>
+        </div>
+      <div class="docker__item">
+        <div class="iconfont">&#xe7c5;</div>
+        <div class="docker__title">订单</div>
+        </div>
+      <div class="docker__item">
+        <div class="iconfont">&#xe609;</div>
+        <div class="docker__title">我的</div>
+      </div>
+  </div>
+  </div>
+</template>
+
+<style lang="scss">
+@import './style/viriables.scss';
+@import './style/mixins.scss';
+.wrapper {
+  position: absolute;
+  left: 0;
+  top: 0;
+  bottom: .5rem;
+  right:0;
+  padding: 0 .18rem;
+}
+.position {
+  position:relative;
+  padding: .16rem .24rem .16rem 0;
+  line-height: .22rem;
+  font-size: .16rem;
+  @include ellipsis;
+  & .position__icon {
+    position:relative;
+    top: .01rem;
+    font-size: .2rem;
+    // margin-right: .08rem;
+  }
+  & .position_notice {
+    position: absolute;
+    right: 0;
+    top: .17rem;
+    font-size: .2rem
+  }
+  color: #333;
+}
+.docker {
+  display: flex;
+  box-sizing: border-box;
+  position: absolute;
+  padding: 0 .18rem;
+  left:0;
+  bottom:0;
+  width: 100%;
+  height: .49rem;
+  border-top: .01rem solid #F1F1F1;
+  color: $content-fontcolor;
+  &__item {
+    flex:1;
+    text-align: center;
+    .iconfont {
+      margin: .07rem 0 .02rem 0;
+      font-size: .18rem;
+    }
+    &--active {
+      color: $content-fontcolor;
+    }
+  }
+  &__title {
+      font-size: .2rem; //浏览器默认最小字体12px, 若直接写10px,展示的还是12px
+      transform: scale(.5, .5); // 垂直和水平缩放 .5
+      transform-origin: center top; //以center和顶部为中心
+  }
+}
+</style>
